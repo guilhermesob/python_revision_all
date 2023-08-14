df89c789925151e0e66814ec6bc959ccedf8c3f3
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-08-13 22:02:58 -0300

    Update removear.py

--- removear.py
+++ removear.py
@@ -1,3 +1,8 @@
 amigos =[['João',25,'Câncer'], ['Maria',23,'Áries'], ['Ana',31,'Aquário']]
 amigos.remove(['João',25,'Câncer'])
 print(amigos)
+
+#or
+# amigos =[['João',25,'Câncer'], ['Maria',23,'Áries'], ['Ana',31,'Aquário']]
+# amigos[2].remove(31)
+# print(amigos)
